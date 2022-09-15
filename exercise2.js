8987826201dbc4abdc815d9225a16fd1211a17ac
Conor Gleeson <conorgleeson2001@gmail.com>
2022-09-15 15:14:27 +0100

    Excercise 2

---
+++ exercise2.js
@@ -0,0 +1,15 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => {
+     const todoTitles = json.map(todo => todo.title); // Complete this code
+     console.log(todoTitles)
+
+    // //  completed.forEach( (todo, index) => {
+    // //   console.log(`${todo.title} - ${todo.completed}`)
+
+    //   console.log(todoTitles)
+    // })
+  })
+  .catch(function(err) { 
+    console.log(err);
+  });
